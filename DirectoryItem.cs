fd3b6c7ebf652b7444d34c0ec35919062d93382b
Arthur Logan <48060299+artlogan@users.noreply.github.com>
2022-04-26 14:00:37 -0500

    Add files via upload

---
+++ DirectoryItem.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace TreeDirectory
+{
+    public class DirectoryItem : TreeViewItem
+    {
+        public static readonly RoutedEvent DirectSelection = EventManager.RegisterRoutedEvent(
+            name: "DirectSelected",
+            routingStrategy: RoutingStrategy.Direct,
+            ownerType: typeof(TreeViewItem),
+            handlerType: typeof(RoutedEventHandler));
+        public event RoutedEventHandler DirectSelected
+        {
+            add { AddHandler(DirectSelection, value); }
+            remove { RemoveHandler(DirectSelection, value); }
+        }
+        public static readonly RoutedEvent DirectExpansion = EventManager.RegisterRoutedEvent(
+           name: "DirectExpanded",
+           routingStrategy: RoutingStrategy.Direct,
+           ownerType: typeof(TreeViewItem),
+           handlerType: typeof(RoutedEventHandler));
+        public event RoutedEventHandler DirectExpanded
+        {
+            add { AddHandler(DirectExpansion, value); }
+            remove { RemoveHandler(DirectExpansion, value); }
+        }
+        public static readonly RoutedEvent DirectCollapse = EventManager.RegisterRoutedEvent(
+           name: "DirectCollapsed",
+           routingStrategy: RoutingStrategy.Direct,
+           ownerType: typeof(TreeViewItem),
+           handlerType: typeof(RoutedEventHandler));
+        public event RoutedEventHandler DirectCollapsed
+        {
+            add { AddHandler(DirectCollapse, value); }
+            remove { RemoveHandler(DirectCollapse, value); }
+        }
+        protected override void OnSelected(RoutedEventArgs e)
+        {
+            RaiseEvent(new(routedEvent: DirectSelection));
+        }
+        protected override void OnExpanded(RoutedEventArgs e)
+        {
+            FillChildren();
+            RaiseEvent(new(routedEvent: DirectExpansion));
+        }
+        protected override void OnCollapsed(RoutedEventArgs e)
+        {
+            ClearChildren();
+            RaiseEvent(new(routedEvent: DirectCollapse));
+        }
+
+
+
+        public readonly string _Address;
+        public readonly string _Name;
+        public readonly string _Type;
+        public readonly long _Size;
+        enum Item_Type
+        { 
+            File, 
+            Directory
+        }
+        Item_Type _Item_Type;
+
+        public DirectoryItem(string addr)
+        {
+            _Address = addr;
+            _Name = _Address.Substring(_Address.LastIndexOf('\\') + 1);
+            _Item_Type = (File.GetAttributes(_Address) & FileAttributes.Directory) == FileAttributes.Directory ? Item_Type.Directory : Item_Type.File;
+            _Type = _Item_Type == Item_Type.Directory ? "Directory" : _Address.Substring(_Address.LastIndexOf('.') + 1);
+            _Size = _Type.Equals("Directory") ? -1 : (new FileInfo(_Address)).Length;
+
+            StackPanel bar = new StackPanel();
+            bar.Orientation = Orientation.Horizontal;
+
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = _Name;
+
+            Image img = new Image();
+
+            TransformedBitmap tbmp = new TransformedBitmap();
+            tbmp.BeginInit();
+            tbmp.Source = (BitmapSource)(new IconBitmapDecoder(new Uri(
+                _Item_Type == Item_Type.File ? @"C:\Users\Owner\source\repos\TreeDirectory\TreeDirectory\document.ico" :
+                @"C:\Users\Owner\source\repos\TreeDirectory\TreeDirectory\folder_open.ico"), 0, 0).Frames[0]);
+            tbmp.Transform = new ScaleTransform(16.0 / tbmp.Source.Height, 16.0 / tbmp.Source.Width);
+            tbmp.EndInit();
+
+            img.Source = tbmp;
+            bar.Children.Add(img);
+            bar.Children.Add(textBlock);
+            Header = bar;
+
+            Fill();
+        }
+
+        public string getName()
+        { 
+            return _Name;
+        }
+
+        public void ClearChildren()
+        {
+            foreach (DirectoryItem child in Items)
+                child.Items.Clear();
+        }
+        public void Fill()
+        {
+            if(_Item_Type == Item_Type.Directory)
+                foreach (string s in Directory.EnumerateFileSystemEntries(_Address))
+                    Items.Add(new DirectoryItem(s));
+        }
+        void FillChildren()
+        {
+            foreach (DirectoryItem child in Items)
+                child.Fill();
+        }
+    }
+
+    public class DirectoryItemBar
+    {
+        public string _Name { get; set; }
+        public string _Type { get; set; }
+        public long _Size { get; set; }
+
+        public readonly DirectoryItem parent;
+
+        public DirectoryItemBar(DirectoryItem di)
+        {
+            _Name = di._Name;
+            _Type = di._Type;
+            _Size = di._Size;
+
+            parent = di;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace TreeDirectory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DirectoryItem current;
+        DirectoryItem clipboard;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DirectoryItem outer = new DirectoryItem(@"E:\Users\Bill\Documents");
+            directory_view.Items.Add(outer);
+            DirectorySelected(outer, new RoutedEventArgs());
+        }
+
+        private void DirectorySelected(object sender, RoutedEventArgs e)
+        {
+            bool fromList = sender.GetType().Equals(typeof(ListViewItem));
+            DirectoryItem caller = fromList ? (DirectoryItem)(((DirectoryItemBar)(((ListViewItem)sender).Content)).parent) : (DirectoryItem)sender;
+            if (caller._Type.Equals("Directory"))
+            {
+                current = caller;
+                if (!fromList)
+                {
+                    directory_list.Items.Clear();
+                    foreach (DirectoryItem d in caller.Items)
+                        directory_list.Items.Add(new DirectoryItemBar(d));
+                    updateLine(caller._Address);
+                    PreviewPanel.Text = "Directory";
+                }
+                else
+                    PreviewPanel.Text = "Directory";
+            }
+            else
+            {
+                PreviewPanel.Text = "";
+                int counter = 0;
+                foreach (string line in System.IO.File.ReadLines(caller._Address))
+                {
+                    if (counter++ >= 128)
+                        break;
+                    PreviewPanel.Text += line + '\n';
+                }
+                if (counter >= 128)
+                    PreviewPanel.Text += "...";
+            }
+        }
+        private void updateLine(string addr)
+        {
+            File_Line.Children.Clear();
+            foreach (string s in addr.Split('\\', StringSplitOptions.RemoveEmptyEntries))
+            { 
+                Button b = new Button();
+                TextBlock t = new TextBlock();
+                t.Text = s;
+                b.Content = t;
+                File_Line.Children.Add(b);
+            }
+        }
+        private void lineClick(object sender, RoutedEventArgs e)
+        {
+            List<Button> buttons = new List<Button>();
+            string addr = "";
+            foreach (Button b in File_Line.Children)
+            {
+                buttons.Add(b);
+                addr += ((TextBlock)(b.Content)).Text + @"\";
+                if (b == (Button)sender)
+                    break;
+            }
+            File_Line.Children.Clear();
+            foreach (Button b in buttons)
+                File_Line.Children.Add(b);
+            DirectorySelected(new DirectoryItem(addr), new RoutedEventArgs());
+        }
+        private void DirectoryExpanded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void DirectoryCollapsed(object sender, RoutedEventArgs e)
+        {
+        }
+        private void OpenDirectory(object sender, RoutedEventArgs e)
+        {
+            DirectoryItemBar dib = (DirectoryItemBar)(((ListViewItem)sender).Content);
+            if (dib._Type.Equals("Directory"))
+            {
+                dib.parent.Fill();
+                DirectorySelected(dib.parent, new RoutedEventArgs());
+            }
+        }
+
+        private void Cut(object sender, RoutedEventArgs e)
+        {
+            Copy(sender, e);
+            Delete(sender, e);
+        }
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            clipboard = current;
+        }
+        private void Paste(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (((DirectoryItem)sender)._Type.Equals("Directory"))
+                (new DirectoryInfo(((DirectoryItem)sender)._Address)).Delete();
+            else
+                (new FileInfo(((DirectoryItem)sender)._Address)).Delete();
+            DirectorySelected(current, new RoutedEventArgs());
+        }
+        private void Duplicate(object sender, RoutedEventArgs e)
+        {
+            Copy(sender, e);
+            Paste(sender, e);
+        }
+        private void NewFolder(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Rename(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Zip(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Print(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
